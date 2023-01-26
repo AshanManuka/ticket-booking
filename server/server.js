8fa413deae7b3528edf7cacca5e752d441c9ef0d
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-01-26 12:10:48 +0530

    import socketIO

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log("Server is Started...")
 })
